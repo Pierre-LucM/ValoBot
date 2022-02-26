a4af933bcd9f68074c49420caec7835f9863ce70
Pierre-Luc Millet <pierre-luc.millet@student.junia.com>
2022-02-26 20:11:37 +0100

    delete useless files

--- src/Commands.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommandList = void 0;
-const Hello_1 = require("./commands/Hello");
-const AccountData_1 = require("./commands/AccountData");
-const MmrData_1 = require("./commands/MmrData");
-exports.CommandList = [Hello_1.Hello, AccountData_1.accountData, MmrData_1.MmrData];
-//# sourceMappingURL=Commands.js.map
